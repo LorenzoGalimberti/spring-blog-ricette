6ba195c63f9c5c369a01066c9eb8c98bdf262430
LorenzoGalimberti <lorenzogalimberti7996@gmail.com>
2023-09-29 10:43:16 +0200

    creazione ricetta

--- src/main/java/com/test/blogricetter/controller/RicettaController.java
+++ src/main/java/com/test/blogricetter/controller/RicettaController.java
@@ -1,17 +1,54 @@
 package com.test.blogricetter.controller;
 
 
+import com.test.blogricetter.model.Ricetta;
+import com.test.blogricetter.repository.RicettaRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
 public class RicettaController {
+    @Autowired
+    private RicettaRepository ricettaRepository;
+    // mostra ricette
     @GetMapping
     public String home(Model model){
+        List<Ricetta> ricetteList=ricettaRepository.findAll();
+        model.addAttribute("ricette",ricetteList);
         return "homepage";
     }
+
+    // crea ricette
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("ricetta", new Ricetta());
+        return "ricette/form"; // template
+    }
+
+    @PostMapping("/create")
+    public String doCreate(@Valid @ModelAttribute("ricetta") Ricetta formRicetta,
+                           BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("categoryList", ricettaRepository.findAll());
+            return "ricette/form";
+        }
+        ricettaRepository.save(formRicetta);
+        return "redirect:/";
+    }
+    // edita ricetta
+    // cancella ricetta
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+
+
+
+
+INSERT INTO ricetta (titolo, ingredienti, url_foto, tempo_preparazione, porzioni, testo_ricetta) VALUES ('Pasta al Pomodoro', '200g di pasta, 400g di pomodori, olio d\'oliva, basilico, sale, pepe', 'https://example.com/pasta.jpg', 30, 2, '1. Cuocere la pasta...\n2. Preparare il sugo di pomodoro...\n3. Servire con basilico e olio d\'oliva.');
